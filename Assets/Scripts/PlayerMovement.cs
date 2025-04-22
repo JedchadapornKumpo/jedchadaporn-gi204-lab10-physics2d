2425e63e1d9b0a349878a76fb4a365aa22dd387f
JedchadapornKumpo <Jedchadaporn.kump@bumail.net>
2025-04-22 12:26:49 +0700

    Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody2D rb2d;
-
-    public float move;
+    Rigidbody2D rb2d;//private variable
+    
+    //walk left-right
+    float move;//store Input from player
     [SerializeField]float speed;
+    
+    //Jump
+    [SerializeField]float jumpForce;
+    [SerializeField]bool isJumping;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,6 +23,30 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //walk left-right
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity  = new Vector2(move * speed, rb2d.linearVelocity.y);
+        //jump
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("Jump!"); //for debugging
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
     }
 }
